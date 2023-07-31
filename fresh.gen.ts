ec50f3bdaea178abc7b2904025220756496c7b6f
Gerald H <empty>
2023-07-31 23:09:49 +0200

    Cleanup: remove generated sample code

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,10 +6,8 @@ import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_app.tsx";
 import * as $2 from "./routes/api/joke.ts";
 import * as $3 from "./routes/chat/[name].tsx";
-import * as $4 from "./routes/greet/[name].tsx";
-import * as $5 from "./routes/index.tsx";
-import * as $$0 from "./islands/Counter.tsx";
-import * as $$1 from "./islands/Messages.tsx";
+import * as $4 from "./routes/index.tsx";
+import * as $$0 from "./islands/Messages.tsx";
 
 const manifest = {
   routes: {
@@ -17,12 +15,10 @@ const manifest = {
     "./routes/_app.tsx": $1,
     "./routes/api/joke.ts": $2,
     "./routes/chat/[name].tsx": $3,
-    "./routes/greet/[name].tsx": $4,
-    "./routes/index.tsx": $5,
+    "./routes/index.tsx": $4,
   },
   islands: {
-    "./islands/Counter.tsx": $$0,
-    "./islands/Messages.tsx": $$1,
+    "./islands/Messages.tsx": $$0,
   },
   baseUrl: import.meta.url,
 };
