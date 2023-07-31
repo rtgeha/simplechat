691e16562e461ca05baf4f128669d2084361d8b0
Gerald H <empty>
2023-07-31 23:03:15 +0200

    Implement simple start page redirecting to chat

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,30 @@
 import { Head } from "$fresh/runtime.ts";
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
+import { Handlers } from "$fresh/server.ts";
+
+export const handler: Handlers = {
+  async GET(req, ctx) {
+    return await ctx.render();
+  },
+  async POST(req, ctx) {
+    const form = await req.formData();
+    const userName = form.get("username")?.toString();
+
+    // Redirect user to chat
+    const headers = new Headers();
+    if (userName) {
+      headers.set("location", "/chat/" + userName);
+    } else {
+      headers.set("location", "/");
+    }
+    
+    return new Response(null, {
+      status: 303, // See Other
+      headers,
+    });
+  },
+};
 
 export default function Home() {
-  const count = useSignal(3);
   return (
     <>
       <Head>
@@ -18,12 +39,11 @@ export default function Home() {
             height="128"
             alt="the fresh logo: a sliced lemon dripping with juice"
           />
-          <h1 class="text-4xl font-bold">Welcome to fresh</h1>
-          <p class="my-4">
-            Try updating this message in the
-            <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-          </p>
-          <Counter count={count} />
+          <h1 class="text-4xl font-bold">Welcome to fresh chat</h1>
+          <form method="post">
+            <input type="text" name="username" value="ANONYMOUS" required/>
+            <button type="submit">Start chatting</button>
+          </form>
         </div>
       </div>
     </>
