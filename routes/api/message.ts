bc5292c2fea14a2c581c50f5b3e3d54d75c5437d
Gerald H <empty>
2023-08-01 17:54:45 +0200

    improve db offline eperience by notifying others before trying to save a message to the database

--- routes/api/message.ts
+++ routes/api/message.ts
@@ -9,13 +9,13 @@ export async function handler(
   const messagesRepo = await MessageRepository.getInstance();
   const message = (await req.json()) as Message;
 
-  // Store in memory and to DB
-  messagesRepo.addMessage(message);
-
   // notify other clients
   const channel = new BroadcastChannel("chat");
   channel.postMessage(message);
   channel.close();
+
+  // Store in memory and to DB
+  messagesRepo.addMessage(message);
 
   return new Response("OK");
 }
